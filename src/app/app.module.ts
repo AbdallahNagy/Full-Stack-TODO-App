571294a3ba128c89bb4ee988de920b9e38794d97
AbdallahNagy <abdallahnagy773@gmail.com>
2023-01-17 13:02:23 +0200

    DO main layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from "./Components/main/main.component";
+import { ListsComponent } from './Components/lists/lists.component';
+import { TasksComponent } from './Components/tasks/tasks.component';
+
+const root: Routes = [
+  // {path: '/', component:}
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainComponent,
+        ListsComponent,
+        TasksComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(root),
+    ]
 })
 export class AppModule { }
